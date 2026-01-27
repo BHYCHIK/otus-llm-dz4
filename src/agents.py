c56177bce1dfac3ecbccbbf29d75864434ac8fd7
Ivan Remen <bhyk@yandex.ru>
2026-01-27 14:24:54 +0300

    Make simple agent with simple tool

--- src/agents.py
+++ src/agents.py
@@ -1,35 +1,73 @@
 from langchain.agents import create_agent
-from langchain.tools import tool
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.runnables import RunnableConfig
 from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.constants import START
+from langgraph.graph import MessagesState, StateGraph
 
 from dotenv import load_dotenv
 import os
 
-load_dotenv('.env')
+from langgraph.prebuilt import ToolNode, tools_condition
 
-@tool
-def get_weather(city: str) -> str:
-    """Get weather for a given city."""
-    print('tool calling')
-    return f"It is snowy in {city}!"
+from tools.rss_collector import rss_collector
+
+load_dotenv('.env')
 
 llm = ChatOpenAI(
     base_url=os.getenv('API_BASE_URL'),
     model='qwen-3-32b',
     temperature=0.1,
-    api_key='APIKEY')
+    api_key=os.getenv('API_KEY'))
 
-agent = create_agent(
-    model=llm,
-    tools=[get_weather],
-    system_prompt="I am wether predictor",
-    
-)
+tools = [rss_collector.last_ai_articles_tool]
 
-# Run the agent
-result = agent.invoke(
-    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
-)
+llm_with_tools = llm.bind_tools(tools)
 
-for message in result['messages']:
-    print(message.content)
+memory = MemorySaver()
+
+def call_model(state: MessagesState):
+    messages = state["messages"]
+    response = llm_with_tools.invoke(messages)
+    return {"messages": response}
+
+workflow = StateGraph(MessagesState)
+workflow.add_node("agent", call_model)
+workflow.add_node("tools", ToolNode(tools))
+
+workflow.add_edge(START, "agent")
+workflow.add_edge("tools", "agent")
+workflow.add_conditional_edges("agent", tools_condition)
+
+app = workflow.compile(checkpointer=memory)
+
+sys_msg = SystemMessage(content="""Ты технический директор холдинга. Пишешь для социальных сетей.
+            "Твой профессиональный интерес управление, искусственный интеллект и надежность." +
+            "Только качественные тексты без маркетингового буллшита.""")
+
+def main():
+    config: RunnableConfig = {
+        'configurable': {'thread_id': 1}
+    }
+
+    inputs: MessagesState = {
+        'messages': [sys_msg,
+                     HumanMessage(content='Напиши статью про искусственный интеллект. Используй последние статьи и новости. Проверь внешние статьи и новости на качество. При написании статьи избегай код. Только русский текст без программирования. Если ссылаешься на внешнюю статью, то прикладывай гиперссылку. Добавь хэштегов'
+                                  )]
+    }
+
+    for event in app.stream(inputs, config=config):
+        if "agent" in event:
+            print(".", end="", flush=True)
+        if "tools" in event:
+            print("[Используем тул] ", end="", flush=True)
+
+    snapshot = app.get_state(config)
+    if snapshot.values["messages"]:
+        last_message = snapshot.values["messages"][-1]
+        if hasattr(last_message, "content"):
+            print(f"\n\n🤖 Ассистент:\n{last_message.content}")
+
+if __name__ == "__main__":
+    main()
---
+++ src/tools/rss_collector/rss_collector.py
@@ -0,0 +1,65 @@
+import feedparser
+import requests
+import time
+from langchain.tools import tool
+from pydantic import BaseModel
+from bs4 import BeautifulSoup
+import time
+
+class OriginalArticleData(BaseModel):
+    title: str
+    link: str
+    full_text: str
+    summary: str
+
+class OriginalArticles(BaseModel):
+    articles: list[OriginalArticleData]
+
+def _parse_habr_article(link: str) -> str:
+    resp = requests.get(link, verify=False)
+    if resp.status_code != 200:
+        raise Exception(f"Cannot get article from habr, status {resp.status_code}")
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    full_text = soup.find('div', class_='article-formatted-body').get_text()
+    return full_text
+
+#@tool
+def last_ai_articles_tool():
+    """
+    Returns json with fresh articles and news.
+
+    json dumps from pydantic class OriginalArticles
+
+    class OriginalArticleData(BaseModel):
+        title: str
+        link: str
+        full_text: str
+        summary: str
+
+    class OriginalArticles(BaseModel):
+        articles: list[OriginalArticleData]
+
+    Returns:
+        str: json with fresh articles and news
+    """
+    print('Calling last_ai_articles_tool')
+    current_utc = time.gmtime()
+
+    rss_feed = 'https://habr.com/ru/rss/hubs/artificial_intelligence/articles/?fl=ru'
+    feed_content = requests.get(rss_feed, verify=False)
+    feed = feedparser.parse(feed_content.content)
+    articles = []
+    for entry in feed['entries']:
+        if ((entry['published_parsed'].tm_year == current_utc.tm_year) and
+                (entry['published_parsed'].tm_yday == current_utc.tm_yday - 1)):
+
+            article = OriginalArticleData(
+                title=entry['title'],
+                link=entry['link'],
+                summary=entry['summary'],
+                full_text=_parse_habr_article(entry['link']),
+            )
+
+            articles.append(article)
+
+    return OriginalArticles(articles=articles).model_dump_json()
